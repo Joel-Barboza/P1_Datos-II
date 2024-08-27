4f1e29ae9ae6122b2e919619c665268d52fe4e4c
Joel-Barboza <joelbp5631@gmail.com>
2024-08-26 23:28:02 -0600

    basic structur for MPointerGC as singleton

--- MPointer/MPointerCG.cpp
+++ MPointer/MPointerCG.cpp
@@ -1,3 +1,23 @@
 //
 // Created by joel-barboza on 26/08/24.
 //
+
+#include "MPointer.h"
+#include "MPointerGC.h"
+
+MPointerGC& MPointerGC::instance()
+{
+    static MPointerGC instance_;
+    return instance_;
+}
+
+MPointerGC::MPointerGC() = default;
+
+MPointerGC::~MPointerGC() = default;
+
+MPointerGC::MPointerGC(const MPointerGC&) = default;
+
+MPointerGC& MPointerGC::operator=(const MPointerGC&) { return *this; }
+
+
+
--- MPointer/MPointerGC.h
+++ MPointer/MPointerGC.h
@@ -4,5 +4,18 @@
 
 #ifndef MPOINTERGC_H
 #define MPOINTERGC_H
+#include "MPointer.h"
+
+class MPointerGC
+{
+public:
+    static MPointerGC& instance();
+
+private:
+    MPointerGC();
+    ~MPointerGC();
+    MPointerGC(const MPointerGC&);
+    MPointerGC& operator=(const MPointerGC&);
+};
 
 #endif //MPOINTERGC_H
